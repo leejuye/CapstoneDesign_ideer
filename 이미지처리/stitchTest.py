08dc8766752c161538f6a10f05ba2e4e22a82c35
orihehe <38060133+orihehe@users.noreply.github.com>
2020-03-25 09:48:28 +0900

    Update stitchTest.py

--- 이미지처리/stitchTest.py
+++ 이미지처리/stitchTest.py
@@ -2,15 +2,20 @@ from stitch import Stitcher
 import imutils
 import cv2
 
-leftImage = cv2.imread(r'./image/left1.jpg')
-rightImage = cv2.imread(r'./image/right1.jpg')
-leftImage = imutils.resize(leftImage, width=400)
-rightImage = imutils.resize(rightImage, width=400)
+def rotateImage(image, deg):
+    dst = cv2.transpose(image)
+    dst = cv2.flip(dst, deg == 90)
+    return dst
+
+leftImage = cv2.imread(r'./image/Bleft.jpg')
+rightImage = cv2.imread(r'./image/Bright.jpg')
+leftImage = rotateImage(imutils.resize(leftImage, width=400), 270)
+rightImage = rotateImage(imutils.resize(rightImage, width=400), 270)
 
 stitcher = Stitcher()
 result = stitcher.stitch([leftImage, rightImage])
 
 cv2.imshow("left Image", leftImage)
 cv2.imshow("right Image", rightImage)
-cv2.imshow("Result", result)
+cv2.imshow("Result", rotateImage(result, 90))
 cv2.waitKey(0)
