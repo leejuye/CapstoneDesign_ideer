5bf73b053da027bd57f6077a1ba08db96ff70522
GippeumLee <dlrlqma006@naver.com>
2020-04-10 04:32:12 -0700

    Save result.jpg

--- SmartEyebodyMirror/modules/default/photo/photo.js
+++ SmartEyebodyMirror/modules/default/photo/photo.js
@@ -6,21 +6,21 @@
 Module.register("photo",{
 	defaults: {
 		text: "photo module test",
-		imageSrc: "/modules/default/photo/background.jpg",
+		imageSrc: "/modules/default/photo/front.jpg",
 	},
 
-	// socketNotificationReceived: function(notification, payload){
-	// 	if(notification === "SUCCESS"){
-	// 		this.config.text = payload;
-	// 	}
-	// 	this.updateDom();
-	// },
+	socketNotificationReceived: function(notification, payload){
+	 	if(notification === "SUCCESS"){
+	 		this.config.text = payload;
+	 	}
+	 	this.updateDom();
+	},
 
-	// start: function() {
-	// 	this.current_user = null;
-	// 	this.sendSocketNotification("CONFIG", this.config);
-	// 	Log.info("Starting module: " + this.name);
-	// },
+	start: function() {
+	 	this.current_user = null;
+	 	this.sendSocketNotification("CONFIG", this.config);
+	 	Log.info("Starting module: " + this.name);
+	},
 
 	getDom: function() {
 		// var wrapper = document.createElement("div");
--- SmartEyebodyMirror/modules/default/photo/test.py
+++ SmartEyebodyMirror/modules/default/photo/test.py
@@ -5,52 +5,49 @@ import sys
 
 # print(cv2.__version__)
 
-def main():
-    frontimg = cv2.imread('./image/front.jpg')
-    sideimg = cv2.imread('./image/side.jpg')
-    bgimg = cv2.imread('./image/background.jpg')
+frontimg = cv2.imread('/home/joy/SmartEyebodyMirror/modules/default/photo/image/front.jpg')
+sideimg = cv2.imread('/home/joy/SmartEyebodyMirror/modules/default/photo/image/side.jpg')
+bgimg = cv2.imread('/home/joy/SmartEyebodyMirror/modules/default/photo/image/background.jpg')
 
-    # img1 - img2 difference
-    difimg = cv2.subtract(bgimg, frontimg)
-    # difimg = cv2.subtract(bgimg, sideimg)
+# img1 - img2 difference
+difimg = cv2.subtract(bgimg, frontimg)
+# difimg = cv2.subtract(bgimg, sideimg)
 
-    gray_difimg = cv2.cvtColor(difimg, cv2.COLOR_BGR2GRAY)
+gray_difimg = cv2.cvtColor(difimg, cv2.COLOR_BGR2GRAY)
 
-    # fastNlMeansDenoising(image file) -> noise reduction
-    gray_difimg = cv2.fastNlMeansDenoising(gray_difimg)
+# fastNlMeansDenoising(image file) -> noise reduction
+gray_difimg = cv2.fastNlMeansDenoising(gray_difimg)
 
-    # bilateralFilter(image, kernel size, color SD, distance SD)
-    gray_difimg = cv2.bilateralFilter(gray_difimg, 3, 75, 75)
+# bilateralFilter(image, kernel size, color SD, distance SD)
+gray_difimg = cv2.bilateralFilter(gray_difimg, 3, 75, 75)
 
-    # adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType, blockSize, C)
-    th1 = cv2.adaptiveThreshold(gray_difimg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 2)
+# adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType, blockSize, C)
+th1 = cv2.adaptiveThreshold(gray_difimg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 2)
 
-    # edge detection
-    img_sobel_x = cv2.Sobel(th1, cv2.CV_64F, 1, 0, ksize=3)
-    img_sobel_x = cv2.convertScaleAbs(img_sobel_x)
+# edge detection
+img_sobel_x = cv2.Sobel(th1, cv2.CV_64F, 1, 0, ksize=3)
+img_sobel_x = cv2.convertScaleAbs(img_sobel_x)
 
-    img_sobel_y = cv2.Sobel(th1, cv2.CV_64F, 0, 1, ksize=3)
-    img_sobel_y = cv2.convertScaleAbs(img_sobel_y)
+img_sobel_y = cv2.Sobel(th1, cv2.CV_64F, 0, 1, ksize=3)
+img_sobel_y = cv2.convertScaleAbs(img_sobel_y)
 
-    img_sobel = cv2.addWeighted(img_sobel_x, 1, img_sobel_y, 1, 0)
+img_sobel = cv2.addWeighted(img_sobel_x, 1, img_sobel_y, 1, 0)
 
-    # image copy (laplacian.copy())
-    # finding contours
-    img_sobel = imutils.resize(img_sobel, width=400)
-    cnts = cv2.findContours(img_sobel.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+# image copy (laplacian.copy())
+# finding contours
+img_sobel = imutils.resize(img_sobel, width=400)
+cnts = cv2.findContours(img_sobel.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    # grab_contours
-    cnts = imutils.grab_contours(cnts)
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]
-    screenCnt = None
+# grab_contours
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]
+screenCnt = None
 
-    # image draw
-    frontimg = imutils.resize(frontimg, width=400)
-    image = cv2.drawContours(frontimg, cnts, -1, (0, 255, 0), 1)
-    # image = cv2.drawContours(sideimg, cnts, -1, (0,255,0), 3)
+# image draw
+frontimg = imutils.resize(frontimg, width=400)
+image = cv2.drawContours(frontimg, cnts, -1, (0, 255, 0), 1)
+# image = cv2.drawContours(sideimg, cnts, -1, (0,255,0), 3)
 
-    cv2.imwrite("result.jpg", cv2.resize(frontimg, (400, 700)))
+cv2.imwrite("/home/joy/SmartEyebodyMirror/modules/default/photo/image/result.jpg", cv2.resize(frontimg, (400, 700)))
 
-if __name__ == "__main__":
-    #time.sleep(5)
-    main()
+
