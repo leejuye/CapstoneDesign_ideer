15faae3f403d041cced2122c4e6879507242b286
BJuyoung <qkdwn0313@gmail.com>
2020-05-01 02:00:46 +0900

    add picam

--- SmartEyebodyMirror/modules/default/photo/test.py
+++ SmartEyebodyMirror/modules/default/photo/test.py
@@ -4,58 +4,70 @@ import imutils
 import sys
 import json
 import os
+import picamera
+import time
 
-def to_node(type, message):
+def to_node(type1, message1, type2, message2):
     try:
-        print(json.dumps({type: message}))
+        print(json.dumps({type1: message1, type2: message2}))
     except Exception:
         pass
     sys.stdout.flush()
 
 curPath = os.path.dirname(os.path.abspath(__file__))
-frontimg = cv2.imread(curPath + '/image/front.jpg')
-sideimg = cv2.imread(curPath + '/image/side.jpg')
-bgimg = cv2.imread(curPath + '/image/background.jpg')
+frontImage = cv2.imread(curPath + '/image/front.jpg')
+sideImage = cv2.imread(curPath + '/image/side.jpg')
+backImage = cv2.imread(curPath + '/image/background.jpg')
 
-# img1 - img2 difference
-difimg = cv2.subtract(bgimg, frontimg)
-# difimg = cv2.subtract(bgimg, sideimg)
 
-gray_difimg = cv2.cvtColor(difimg, cv2.COLOR_BGR2GRAY)
+def contour(backImage, image):
+    # img1 - img2 difference
+    difImg = cv2.subtract(backImage, image)
+    grayDifImg = cv2.cvtColor(difImg, cv2.COLOR_BGR2GRAY)
 
-# fastNlMeansDenoising(image file) -> noise reduction
-gray_difimg = cv2.fastNlMeansDenoising(gray_difimg)
+    # fastNlMeansDenoising(image file) -> noise reduction
+    grayDifImg = cv2.fastNlMeansDenoising(grayDifImg)
 
-# bilateralFilter(image, kernel size, color SD, distance SD)
-gray_difimg = cv2.bilateralFilter(gray_difimg, 3, 75, 75)
+    # bilateralFilter(image, kernel size, color SD, distance SD)
+    grayDifImg = cv2.bilateralFilter(grayDifImg, 4, 75, 75)
 
-# adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType, blockSize, C)
-th1 = cv2.adaptiveThreshold(gray_difimg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 2)
+    # adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType, blockSize, C)
+    th = cv2.adaptiveThreshold(grayDifImg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 2)
 
-# edge detection
-img_sobel_x = cv2.Sobel(th1, cv2.CV_64F, 1, 0, ksize=3)
-img_sobel_x = cv2.convertScaleAbs(img_sobel_x)
+    # edge detection
+    imgSobelX = cv2.Sobel(th, cv2.CV_64F, 1, 0, ksize=3)
+    imgSobelX = cv2.convertScaleAbs(imgSobelX)
+    imgSobelY = cv2.Sobel(th, cv2.CV_64F, 0, 1, ksize=3)
+    imgSobelY = cv2.convertScaleAbs(imgSobelY)
+    imgSobel = cv2.addWeighted(imgSobelX, 1, imgSobelY, 1, 0)
 
-img_sobel_y = cv2.Sobel(th1, cv2.CV_64F, 0, 1, ksize=3)
-img_sobel_y = cv2.convertScaleAbs(img_sobel_y)
+    # resize image
+    imgSobel = imutils.resize(imgSobel, width=400)
 
-img_sobel = cv2.addWeighted(img_sobel_x, 1, img_sobel_y, 1, 0)
+    # finding contours
+    cnts = cv2.findContours(imgSobel.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-# image copy (laplacian.copy())
-# finding contours
-img_sobel = imutils.resize(img_sobel, width=400)
-cnts = cv2.findContours(img_sobel.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # grab_contours
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]
+    screenCnt = None
 
-# grab_contours
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]
-screenCnt = None
+    # image draw
+    image = imutils.resize(image, width=400)
+    image = cv2.drawContours(image, cnts, -1, (0, 255, 0), 1)
 
-# image draw
-frontimg = imutils.resize(frontimg, width=400)
-image = cv2.drawContours(frontimg, cnts, -1, (0, 255, 0), 1)
-# image = cv2.drawContours(sideimg, cnts, -1, (0,255,0), 3)
+    return image, cnts
 
-cv2.imwrite(curPath + "/image/result.jpg", cv2.resize(frontimg, (400, 700)))
+cam = picamera.PiCamera()
+cam.start_preview(fullscreen=False, window=(20, 100, 400, 700))
+time.sleep(10)
+cam.stop_preview()
+cam.capture(curPath + "/image/cam.jpg")
 
-to_node("status", "result")
+frontContour, cntsFront = contour(backImage, frontImage)
+sideContour, cntsSide = contour(backImage, sideImage)
+
+cv2.imwrite(curPath + "/image/frontContour.jpg", cv2.resize(frontContour, (400, 700)))
+cv2.imwrite(curPath + "/image/sideContour.jpg", cv2.resize(sideContour, (400, 700)))
+
+to_node("front", "frontContour", "side", "sideContour")
